9b7ccb53806b79e6aac79036417c43be69efef77
niclas <bergerbtw@gmail.com>
2023-05-01 19:21:27 +0200

    feat: Navigation Bar

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <main>
+            <div className="p-3 border-b border-slate-100 flex justify-between items-center">
+                <div className="hidden md:block">
+                    <h1>Great Habits</h1>
+                </div>
+
+                <div>
+                    <NavigationMenu>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <Link href="/product" legacyBehavior passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Product
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+
+                            <NavigationMenuItem>
+                                <Link href="/pricing" legacyBehavior passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Pricing
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+
+                </div>
+
+                <div className="flex gap-1">
+                    <Link href="/auth">
+                        <Button variant="ghost">Sign In</Button>
+                    </Link>
+
+                    <Link href="/auth">
+                        <Button variant="default">Sign Up</Button>
+                    </Link>
+                </div>
+            </div>
+
+            {children}
+        </main>
+    )
+}
