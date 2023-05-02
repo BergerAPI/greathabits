375d13e24797307489018dc43fe2ad6dc6529197
niclas <bergerbtw@gmail.com>
2023-05-02 22:30:24 +0200

    feat: better practices

--- components/todo-view.tsx
+++ components/todo-view.tsx
@@ -2,7 +2,7 @@
 
 import { Database } from "@/lib/database";
 import { useSupabase } from "@/lib/supabase-provider";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import Todo from "./todo";
 
 type Todos = Database['public']['Tables']['todos']['Row']
@@ -15,41 +15,54 @@ const TodoView: FC<Props> = ({ initialTodos }) => {
     const [todos, setTodos] = useState<Todos[]>(initialTodos)
     const { supabase } = useSupabase()
 
+    const sortedTodos = useMemo(() => {
+        return todos
+            .sort((a, b) => a.id - b.id)
+            .sort((a, b) => (a.checked === b.checked) ? 0 : a.checked ? -1 : 1)
+    }, [todos])
+
     useEffect(() => {
 
-        // When a new todo is added
-        supabase.channel("todos").on("postgres_changes", {
-            event: "INSERT",
-            schema: "public",
-            table: "todos"
-        }, (payload) => {
-            setTodos([...todos, payload.new as Todos])
-        }).subscribe()
+        // When a new todo is added, edited or deleted
+        supabase.channel("todos")
+            .on("postgres_changes", {
+                event: "INSERT",
+                schema: "public",
+                table: "todos"
+            }, (payload) => {
+                setTodos([...todos, payload.new as Todos])
+            })
+            .on("postgres_changes", {
+                event: "DELETE",
+                schema: "public",
+                table: "todos"
+            }, (payload) => {
+                setTodos(todos.filter(it => it.id !== payload.old.id))
+            })
+            .on("postgres_changes", {
+                event: "UPDATE",
+                schema: "public",
+                table: "todos",
+            }, async (_) => {
+                const newTodos = await supabase.from("todos").select()
 
-        // When a todo is updated
-        supabase.channel("todos").on("postgres_changes", {
-            event: "UPDATE",
-            schema: "public",
-            table: "todos",
-        }, async (_) => {
-            const newTodos = await supabase.from("todos").select()
+                setTodos(newTodos.data!!)
+            }).subscribe()
 
-            setTodos(newTodos.data!!)
-        }).subscribe()
+        return () => {
+            supabase.channel("todos").unsubscribe()
+        }
     }, [])
 
     return <>
-        {todos
-            .sort((a, b) => a.id - b.id)
-            .sort((a, b) => (a.checked === b.checked) ? 0 : a.checked ? -1 : 1)
-            .map(todo => {
-                return <Todo
-                    checked={todo.checked}
-                    description={todo.description}
-                    title={todo.title}
-                    id={todo.id}
-                    key={`${todo.id}`} />
-            })}
+        {sortedTodos.map(todo => {
+            return <Todo
+                checked={todo.checked}
+                description={todo.description}
+                title={todo.title}
+                id={todo.id}
+                key={`${todo.id}`} />
+        })}
     </>
 }
 
