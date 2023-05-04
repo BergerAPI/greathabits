d398d1a1ac48df489f71194545272da86f552c5b
niclas <bergerbtw@gmail.com>
2023-05-04 22:12:04 +0200

    feat: Icons in the context menu

--- components/todo.tsx
+++ components/todo.tsx
@@ -4,9 +4,10 @@ import { Database } from "@/lib/database";
 import { Checkbox } from "./ui/checkbox";
 import { FC } from "react";
 import { useSupabase } from "@/lib/supabase-provider";
-import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "./ui/context-menu";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSeparator, ContextMenuTrigger } from "./ui/context-menu";
 import { Label } from "./ui/label";
 import { Badge } from "./ui/badge";
+import { ToggleLeft, Trash } from "lucide-react";
 
 type Todos = Database['public']['Tables']['todos']['Row']
 
@@ -71,8 +72,15 @@ const Todo: FC<Props> = ({ checked, id, title, description }) => {
                 </div>
             </ContextMenuTrigger>
             <ContextMenuContent>
-                <ContextMenuItem onClick={onToggle}>Toggle</ContextMenuItem>
-                <ContextMenuItem onClick={onDelete}>Delete</ContextMenuItem>
+                <ContextMenuItem onClick={onToggle}>
+                    <ToggleLeft className="mr-2 h-4 w-4" />
+                    Toggle
+                </ContextMenuItem>
+                <ContextMenuSeparator />
+                <ContextMenuItem onClick={onDelete} className="text-red-600">
+                    <Trash className="mr-2 h-4 w-4" />
+                    Delete
+                </ContextMenuItem>
             </ContextMenuContent>
         </ContextMenu>
     </>
